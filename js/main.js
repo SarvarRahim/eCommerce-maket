b7900ddb8f2a86645ed7363ef4e9b738e009a846
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-08-06 09:34:11 +0500

    some bugs are fixed

--- js/main.js
+++ js/main.js
@@ -35,3 +35,47 @@ var elNav = document.querySelector('.sitenav');
 elOpenNav.addEventListener('click', function() {
   elNav.classList.remove('sitenav--open');
 });
+
+
+
+
+// PRODUCT GRID AND LIST VIEWS
+
+var elGridViewButton = document.querySelector(".product-header__grid-view");
+var elListViewButton = document.querySelector(".product-header__list-view");
+var outComeList = document.querySelector(".outcomes__list");
+var outcomeItems = document.querySelectorAll(".outcome");
+
+// FUNCTION TO SWITCHINIG GRID VIEW
+function switchOutcomeToGridView(params) {
+  outComeList.classList.remove("outcomes__list--list");
+  outComeList.classList.add("outcomes__list--grid");
+  outcomeItems.forEach(function (outcomeItem){
+    outcomeItem.classList.remove("outcome--list");
+    outcomeItem.classList.add("outcome--grid");
+
+  })
+}
+
+// FUNCTION TO SWITCHINIG LIST VIEW
+
+function switchOutcomeToListView(params) {
+  outComeList.classList.remove("outcomes__list--grid");
+  outComeList.classList.add("outcomes__list--list");
+  outcomeItems.forEach(function (outcomeItem){
+    outcomeItem.classList.remove("outcome--grid");
+    outcomeItem.classList.add("outcome--list");
+
+  })
+}
+
+if (elGridViewButton) {
+    elGridViewButton.addEventListener("click", switchOutcomeToGridView);
+
+
+}
+
+if (elListViewButton){
+  elListViewButton.addEventListener("click", switchOutcomeToListView);
+}
+
